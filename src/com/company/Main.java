954bee65d028df1c7299135aa38f0e9f34c57a84
Aesir <a00824388@itesm.mx>
2020-05-01 14:59:52 -0500

    First git commit with Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //PLEASE UPLOAD AND WORK IN COMMIT
+        System.out.println("TESTING output and commits");
+
+    }
+}
